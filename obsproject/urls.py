4d8925ccf01df206da340b13944e466fe94fb78b
Shalini Yadav <145842179+shaliniyadav09@users.noreply.github.com>
2025-10-23 12:10:34 +0530

    Fix indentation for static media URL pattern

--- obsproject/urls.py
+++ obsproject/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
      path('userapp/' , include('userapp.userappurls')),
 ]
 if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
