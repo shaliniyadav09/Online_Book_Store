a823a761791f90157e6ace2fb4c0c1a8989a7382
Shalini Yadav <145842179+shaliniyadav09@users.noreply.github.com>
2025-10-23 12:04:31 +0530

    Add WhiteNoiseMiddleware to MIDDLEWARE settings

--- obsproject/settings.py
+++ obsproject/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # ✅ Add this line
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
